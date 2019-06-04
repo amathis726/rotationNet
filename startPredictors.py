bd383c1f427465d50f9c3ce04f98cce550989c37
amathis726 <44485445+amathis726@users.noreply.github.com>
2019-06-04 08:06:51 -0700

    Add files via upload

--- startPredictors.py
+++ startPredictors.py
@@ -14,12 +14,12 @@ path = Path('./predictCaptures')
 # pyperclip.copy(os.path.abspath('captures'))
 printMessage = True
 nview = 160
-maxImages = 20
+maxImages = 50
 numImages = 0
 newNumImages = 0
 reset = 'False'
 pyperclip.copy('13 0 0 0')
-vcand = np.load('vcand_case3.npy') # View candidates for case 160 view capture setup
+vcand = np.load('vcand_case3.npy') # View candidates for 160 views captured setup
 
 # Load and process view order dictionary.
 viewOrd = pickle.load(open("viewOrderDict.pkl", "rb"))
@@ -44,7 +44,7 @@ try:
             reset = pyperclip.paste()
             xb = torch.FloatTensor()
             # User wants to reset or max number of views reached, so delete all images and start over
-            if numImages == maxImages or reset == 'True':
+            if numImages == maxImages or reset == 'True 0 0 0':
                 print('\nRESETTING...')
                 print('Final prediction:', values[np.argmax(counts)])
                 numImages = 0
@@ -67,7 +67,15 @@ try:
                         printMessage = False
                     newNumImages = len(os.listdir(path))
                     reset = pyperclip.paste()
-                    if reset == 'True':
+                    if reset == 'True 0 0 0':
+                        # print('Deleting...')
+                        reset = 'False'
+                        for filename in os.listdir(path):
+                            filepath = os.path.join(path, filename)
+                            try:
+                                shutil.rmtree(filepath)
+                            except OSError:
+                                os.remove(filepath)
                         break
                 numImages = newNumImages
                 printMessage = True
@@ -82,55 +90,40 @@ try:
                         b,_ = batch
                         xb = torch.cat((xb, b), dim=0)
                         # print('Data sucessfully added!')
-
-                # for i in range(len(os.listdir(path))):
-                #     fname = 'view_'+ str(i+1) + '.png'
-                #     if i == 0:
-                #         print('Scanning...')
-                #     img = open_image(path/fname)
-                #     print('Adding data...')
-                #     batch = learn.data.one_item(img)
-                #     b,_ = batch
-                #     xb = torch.cat((xb, b), dim=0)
-                #     print('Data sucessfully added!')
             
             except OSError:
                 print('Warning: Data is unuseable. Continue scanning object...')
                 printMessage = True
                 break
-                               
-            xb = [xb]
-            learn.model.eval()
-            out = learn.model(*xb)
-            batch_size = xb[0].size(0)
-            num_classes = int(out.size( 1 )/ nview) - 1
-            out = out.view( -1, num_classes + 1 )
-            out = torch.nn.functional.log_softmax( out, dim=1 )
-            out = out[ :, :-1 ] - torch.t( out[ :, -1 ].repeat(1, out.size(1)-1).view(out.size(1)-1, -1) )
-            predictions = []
-            posePreds = []
-            for i in range(batch_size): 
-                maxVals, idxs = torch.max(out[i*nview:i*nview+nview], dim=1)
-                predictions.append(idxs[torch.argmax(maxVals)].item())
-                # posePreds.append(torch.argmax(maxVals).item())
-                posePred = torch.argmax(maxVals).item()
-                # print(predictions, posePreds)
-            values, counts = np.unique(predictions, return_counts=True)
-            if predictions[-1] == values[np.argmax(counts)]:
-                pose = vcand[viewOrd[predictions[-1]]][posePred]
-                if pose < 80:
-                    rotation = str( '0 ' + str(-16*(pose//16)-16) + ' ' + str(-22.5*(pose-((pose//16)*16))) )
+
+            if xb.shape[0] != 0:                 
+                xb = [xb]
+                learn.model.eval()
+                out = learn.model(*xb)
+                batch_size = xb[0].size(0)
+                num_classes = int(out.size( 1 )/ nview) - 1
+                out = out.view( -1, num_classes + 1 )
+                out = torch.nn.functional.log_softmax( out, dim=1 )
+                out = out[ :, :-1 ] - torch.t( out[ :, -1 ].repeat(1, out.size(1)-1).view(out.size(1)-1, -1) )
+                predictions = []
+                posePreds = []
+                for i in range(batch_size): 
+                    maxVals, idxs = torch.max(out[i*nview:i*nview+nview], dim=1)
+                    predictions.append(idxs[torch.argmax(maxVals)].item())
+                    # posePreds.append(torch.argmax(maxVals).item())
+                    posePred = torch.argmax(maxVals).item()
+                    # print(predictions, posePreds)
+                values, counts = np.unique(predictions, return_counts=True)
+                if predictions[-1] == values[np.argmax(counts)]:
+                    pose = vcand[viewOrd[predictions[-1]]][posePred]
+                    rotation = str( '0 ' + str(16*(pose//16)-80) + ' ' + str(22.5*(pose-((pose//16)*16))) )
+                    preds = str(values[np.argmax(counts)]) + ' ' + str(rotation)
+                    print('Predictions: ', preds)
+                    print(pose)
+                    pyperclip.copy(preds)
                 else:
-                    pose = pose - 80
-                    rotation = str( '0 ' + str(16*(pose//16)+16) + ' ' + str(-22.5*(pose-((pose//16)*16))) )
-                    pose = pose + 80
-                preds = str(values[np.argmax(counts)]) + ' ' + str(rotation)
-                print('Predictions: ', preds)
-                print(pose)
-                pyperclip.copy(preds)
-            else:
-                print('\nAmbiguous view. Continue scanning object...')
-                print('Current evaluation', predictions[-1], 'does not match best prediction.')
+                    print('\nAmbiguous view. Continue scanning object...')
+                    print('Current evaluation', predictions[-1], 'does not match best prediction.')
 
                             
 
